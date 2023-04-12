e785f4ff53c6ae610a2944184f458013a5fd63e5
ELISHIFA <elishwanga@gmail.com>
2023-04-12 18:40:08 +0000

    input/output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,6 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initializing the new Student"""
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
