65e19cf8c63b6a2240dbd1b0934ec0fde8267ed1
ELISHIFA <elishwanga@gmail.com>
2023-04-12 17:52:08 +0000

    input/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 #0-read_file.py
 """
-Defines the read_file function
+   Defines the read_file function
 """
 
 
 def read_file(filename=""):
-    """""reads and prints text file(UTF8) to stdout"""
+    """"reads and prints text file(UTF8) to 
+    standard output
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
