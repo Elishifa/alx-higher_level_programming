8a376aa2f0fcbf45c5c4cf9a08a6f2f280c06627
ELISHIFA <elishwanga@gmail.com>
2023-04-13 19:12:36 +0000

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """A representation of BaseGeometry class"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Rectangle class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """New Rectangle initialization
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
