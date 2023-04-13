94642bb60e4e8c33ca4a8136e3d7d0ca0c941ee5
ELISHIFA <elishwanga@gmail.com>
2023-04-13 16:47:56 +0000

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-    Function returns the list of 
-    available attributes and methods of an object
+Function returns the list of available attributes and methods of an object
 """
 
 
