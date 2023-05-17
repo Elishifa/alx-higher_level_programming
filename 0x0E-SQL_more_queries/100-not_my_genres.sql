66b1fc7f4c41d7b14be87f7dbfc0a914e54007ac
ELISHIFA <elishwanga@gmail.com>
2023-05-17 18:20:23 +0000

    more database scripts

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,6 +9,6 @@ WHERE g.name NOT IN (
       FROM tv_genres g
       INNER JOIN tv_show_genres m ON g.id = m.genre_id
       INNER JOIN tv_shows s ON m.show_id = s.id
-      WHERE t.title = 'Dexter'
+      WHERE s.title = 'Dexter'
 )
 ORDER BY g.name ASC;
