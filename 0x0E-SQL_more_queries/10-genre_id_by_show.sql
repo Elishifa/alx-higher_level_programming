d2845a462cc79318cc16660d3a350aa4e9e4f683
ELISHIFA <elishwanga@gmail.com>
2023-05-17 19:55:04 +0000

    more database scripts

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,6 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+	FROM tv_shows_genres
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	ORDER BY tv_shows.title, tv_show_genres.genre_id;
