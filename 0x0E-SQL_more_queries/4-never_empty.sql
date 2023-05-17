f9303af26fffbbc7da278b2b64ab6ad8b2d803d7
ELISHIFA <elishwanga@gmail.com>
2023-05-17 18:19:00 +0000

    more database scripts

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,6 @@
 -- a script that creates the table id_not_null
 -- If the table id_not_null already exists, your script should not fail
-CREATE TABLE IF NOT EXITS id_not_null (
+CREATE TABLE IF NOT EXISTS id_not_null (
 id INT DEFAULT 1,
 name VARCHAR(256)
 );
